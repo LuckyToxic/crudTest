31174d0a389ce2eaa72c3df02072241c2bf985c8
Evgeniy <151203550+LuckyToxic@users.noreply.github.com>
2025-03-30 17:32:43 +0300

    Update Header.tsx

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -28,7 +28,6 @@ export default function Header() {
           px: { xs: 2, sm: 4, md: 6 },
         }}
       >
-        {/* Лого и название */}
         <Box
           component={Link}
           to="/"
@@ -63,7 +62,6 @@ export default function Header() {
         <Box sx={{ display: { xs: "none", sm: "flex" }, gap: 2 }}>
           <Button
             component={Link}
-            to="/news"
             color="inherit"
             sx={{ textTransform: "none", fontWeight: 500 }}
           >
@@ -71,7 +69,6 @@ export default function Header() {
           </Button>
           <Button
             component={Link}
-            to="/categories"
             color="inherit"
             sx={{ textTransform: "none", fontWeight: 500 }}
           >
@@ -79,7 +76,6 @@ export default function Header() {
           </Button>
           <Button
             component={Link}
-            to="/about"
             color="inherit"
             sx={{ textTransform: "none", fontWeight: 500 }}
           >
